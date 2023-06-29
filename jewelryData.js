9d7fd6fa61e7e7017b47d67a9ebca1a51777a108
Edwin Jon Hontiveros <116496725+edwinjonhontiveros-bpsu@users.noreply.github.com>
2023-06-29 20:57:37 +0800

    aight prod is done

---
+++ jewelryData.js
@@ -0,0 +1,43 @@
+// specifically for jewelryData.xml
+
+function load() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) { disp(this); }
+  };
+  xhttp.open("GET", "jewelryData.xml", true);
+  xhttp.send();
+}
+
+function disp(xml) {
+  var xmlDoc = xml.responseXML;
+  var records = xmlDoc.getElementsByTagName("jewelry");
+  var out = "";
+  for (var i = 0; i < records.length; i++) {
+    var name = records[i].getElementsByTagName("name")[0].textContent;
+    var price = records[i].getElementsByTagName("price")[0].textContent;
+    var desc = records[i].getElementsByTagName("desc")[0].textContent;
+    var currency = "₱";
+    var img = name.toLowerCase().replace(/ /g, "_") + ".webp";
+
+    out += "<div class='recordDetails'>" + 
+      "<h2>Name</h2>" +
+        "<p>" + name + "</p>" +
+      "<h2>Price</h2>" +
+        "<p>" + currency + price + "</p>" +
+      "<h2>Description</h2>" + 
+        "<p>" + desc + "</p><br>" +
+      "<img src='media/" + img + "' alt='" + name + "'>" +
+    "</div>";
+  }
+
+  out += "<div class='recordDetails'>" + 
+    "<p><b>" +
+      "For more information, head over to one of our branches or call us at " +
+      "047-092-BHEM for assistance." +
+    "</b></p>" +
+  "</div>";
+
+  document.getElementById("showJewelries").style.display = "block";
+  document.getElementById("showJewelries").innerHTML = out;
+}
